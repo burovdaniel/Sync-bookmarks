51407cbfafae2138bf044feadc21553bdbcb2b72
burovdaniel <burovdaniel@gmail.com>
2021-10-02 18:31:13 +0100

    if folder number add

--- Sync-BOOK.py
+++ Sync-BOOK.py
@@ -3,14 +3,14 @@ with open('/home/daniel/.config/BraveSoftware/Brave-Browser/Default/Bookmarks') 
 	lines=f.readlines()
 #tag children indcats start of folder
 
-#adds line number tag children is found to list folder
+#adds line number tag children is found to list folder and folder name
 i=0
 folder=[]
+markers=[]
 while i < len(lines):
 	if lines[i] == lines[5]:
 		folder.append(i)
+	if lines[i] == lines[46]:
+		markers.append(i)	
 	i+=1
-
-#print(folder)	
-print(lines[folder[2]])
-	
+print(markers)
